ca7bbf310ec3a6279fe33dca58a32edde7aa4b6a
Ostin63 <Ostin63@users.noreply.github.com>
2018-10-13 12:26:39 +0300

    модальные окна

--- assets/js/modal-feedback.js
+++ assets/js/modal-feedback.js
@@ -19,6 +19,29 @@ var formCons = popupCons.querySelector("form");
 var yoursNameCons = popupCons.querySelector(".form-name");
 var phoneCons = popupCons.querySelector(".form-phone");
 
+var popupProject = document.querySelector(".modal-project");
+var linkProject = document.querySelector(".button-project");
+var closeProject = popupProject.querySelector(".modal-close");
+
+var formProject = popupProject.querySelector("form");
+var yoursNameProject = popupProject.querySelector(".form-name");
+var phoneProject = popupProject.querySelector(".form-phone");
+
+
+var formMain = document.querySelector(".main-form");
+var yoursNameMain = formMain.querySelector(".form-name");
+var phoneMain = formMain.querySelector(".form-phone");
+
+
+linkProject.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupProject.classList.add("modal-visible");
+  overLay.classList.add("modal-visible");
+
+});
+
+
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-visible");
@@ -32,15 +55,12 @@ linkCons.addEventListener("click", function (evt) {
   overLay.classList.add("modal-visible");
 
 });
-/*
-linkThank.addEventListener(function (evt) {
-  evt.preventDefault();
-  popupThank.classList.add("modal-visible");
-  overLay.classList.add("modal-visible");
-  popup.classList.remove("modal-visible");
-  popupCons.classList.remove("modal-visible");
 
-});*/
+closeProject.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupProject.classList.remove("modal-visible");
+  overLay.classList.remove("modal-visible");
+});
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -58,6 +78,53 @@ closeThank.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupThank.classList.remove("modal-visible");
   overLay.classList.remove("modal-visible");
+});
+
+formMain.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  if (!yoursNameMain.value || !phoneMain.value) {
+    phoneMain.classList.add("empty-value-error");
+    yoursNameMain.classList.add("empty-value-error")
+  } else {
+    $.ajax({
+      url: 'https://echo.htmlacademy.ru',
+      type: 'post',
+      data: $(formMain).serialize(),
+      success: function () {
+        popupThank.classList.add("modal-visible");
+        overLay.classList.add("modal-visible");
+      }
+    });
+  }
+});
+
+formProject.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  if (!yoursNameProject.value) {
+    yoursNameProject.classList.add("empty-value-error")
+  }
+  if (!phone.value) {
+    phoneProject.classList.add("empty-value-error")
+  }
+
+  if (!yoursNameProject.value || !phoneProject.value) {
+    popupProject.classList.remove("modal-error");
+    popupProject.offsetWidth = popupProject.offsetWidth;
+    popupProject.classList.add("modal-error");
+  } else {
+    $.ajax({
+      url: 'https://echo.htmlacademy.ru',
+      type: 'post',
+      data: $(formProject).serialize(),
+      success: function () {
+        popupThank.classList.add("modal-visible");
+        overLay.classList.add("modal-visible");
+        popupProject.classList.remove("modal-visible");
+      }
+    });
+  }
 });
 
 form.addEventListener("submit", function (evt) {
